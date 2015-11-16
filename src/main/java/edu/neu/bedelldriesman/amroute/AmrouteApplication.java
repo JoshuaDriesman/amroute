6ef6e3e3233590b90deed2e01729137d2327c07a
Joshua Driesman <jdriesman@live.com>
2015-11-16 18:05:44 -0500

    Initial Commit

---
+++ src/main/java/edu/neu/bedelldriesman/amroute/AmrouteApplication.java
@@ -0,0 +1,12 @@
+package edu.neu.bedelldriesman.amroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmrouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmrouteApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/neu/bedelldriesman/amroute/AmrouteApplicationTests.java
@@ -0,0 +1,18 @@
+package edu.neu.bedelldriesman.amroute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AmrouteApplication.class)
+@WebAppConfiguration
+public class AmrouteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
