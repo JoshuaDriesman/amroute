09f393c480dc0489fab6777cc65f9cf74f679fcd
Joshua Driesman <jdriesman@live.com>
2015-11-17 00:02:05 -0500

    Implemented city.

--- src/main/java/edu/neu/bedelldriesman/amroute/entitybeans/City.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/entitybeans/City.java
@@ -1,7 +1,62 @@
 package edu.neu.bedelldriesman.amroute.entitybeans;
 
+import java.util.Objects;
+
 /**
  * Created by Joshua Driesman on 11/16/2015.
  */
 public class City {
+    private int cityId;
+    private String name;
+    private String state;
+    private String region;
+
+    public City(int cityId, String name, String state, String region) {
+        this.cityId = cityId;
+
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(state);
+        Objects.requireNonNull(region);
+        this.name = name;
+        this.state = state;
+        this.region = region;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        Objects.requireNonNull(name);
+
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        Objects.requireNonNull(state);
+
+        this.state = state;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        Objects.requireNonNull(region);
+
+        this.region = region;
+    }
 }
