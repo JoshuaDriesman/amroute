aead35d780925c20cec72b8f2a757a764b357866
Joshua Driesman <jdriesman@live.com>
2015-11-16 23:06:27 -0500

    Implemented route bean

--- src/main/java/edu/neu/bedelldriesman/amroute/entitybeans/Route.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/entitybeans/Route.java
@@ -1,7 +1,24 @@
 package edu.neu.bedelldriesman.amroute.entitybeans;
 
+import java.util.Objects;
+
 /**
  * Created by Joshua Driesman on 11/16/2015.
  */
 public class Route {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public Route(String name) {
+        Objects.requireNonNull(name);
+
+        this.name = name;
+    }
 }
