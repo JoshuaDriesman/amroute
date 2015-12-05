a1875f307a590ad917d7ca9f4835b4a906b1fc06
Joshua Driesman <jdriesman@live.com>
2015-12-05 18:10:18 -0500

    Added function to get city names. Added procedure to get stops in time range.

--- src/main/sql/storedProcedures.sql
+++ src/main/sql/storedProcedures.sql
@@ -1,5 +1,31 @@
 use amroute;
 
+/* Functions */
+DROP FUNCTION IF EXISTS cityNameForId;
+DELIMITER $$
+CREATE FUNCTION cityNameForId(cId INT)
+RETURNS VARCHAR(45)
+BEGIN
+DECLARE cityName VARCHAR(45);
+DECLARE noneFound INT DEFAULT FALSE;
+DECLARE csr CURSOR FOR 
+	SELECT name FROM cities WHERE idCities = cId;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET noneFound = TRUE;
+
+OPEN csr;
+FETCH csr INTO cityName;
+CLOSE csr;
+
+IF noneFound THEN
+	SET cityName = null;
+END IF;
+
+RETURN cityName;
+END
+$$ DELIMITER ;
+
+/* Procedures */
+
 /* Find all routes for a certain city */
 DROP PROCEDURE IF EXISTS getServingRoutes;
 DELIMITER $$
@@ -54,3 +80,17 @@ SELECT DISTINCT cities.name FROM (schedule JOIN cities
 	ON schedule.termination = cities.idCities AND schedule.route = r);
 END
 $$ DELIMITER ;
+
+/* Procedure to get all that depart in time range */
+DROP PROCEDURE IF EXISTS getStopsInTimeRange;
+DELIMITER $$
+CREATE PROCEDURE getStopsInTimeRange(IN lower TIME, upper TIME)
+BEGIN
+SELECT route, cityNameForId(origin) 'From', cityNameForId(termination) 'To', cities.name 'Stop', time
+	FROM (schedule JOIN schedulecities ON schedule.idSchedule = schedulecities.scheduleId AND
+			schedulecities.time > lower AND schedulecities.time < upper) 
+		JOIN cities ON cities.idCities = schedulecities.cityId;
+END
+$$ DELIMITER ;
+
+CALL getStopsInTimeRange(TIME '3:00:00', TIME '12:00:00');
