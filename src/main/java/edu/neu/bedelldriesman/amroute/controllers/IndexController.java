5e6fd9213e61b198f57d662362b8976d824f99af
Joshua Driesman <jdriesman@live.com>
2015-12-06 15:21:23 -0500

    Implemented index controller

--- src/main/java/edu/neu/bedelldriesman/amroute/controllers/IndexController.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/controllers/IndexController.java
@@ -1,9 +1,17 @@
 package edu.neu.bedelldriesman.amroute.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Joshua Driesman on 12/6/2015.
  * <p>
  * Copyright 2015 Joshua Driesman, All rights reserved.
  */
+@Controller
 public class IndexController {
+    @RequestMapping("/")
+    public String getIndex() {
+        return "index";
+    }
 }
