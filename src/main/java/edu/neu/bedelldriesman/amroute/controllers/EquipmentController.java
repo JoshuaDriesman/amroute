4438325681a0ca1512f30c13b100a1e2cd2fea4d
Joshua Driesman <jdriesman@live.com>
2015-12-06 15:04:20 -0500

    Initial commit for equipment controller

---
+++ src/main/java/edu/neu/bedelldriesman/amroute/controllers/EquipmentController.java
@@ -0,0 +1,12 @@
+package edu.neu.bedelldriesman.amroute.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Joshua Driesman on 12/6/2015.
+ * <p>
+ * Copyright 2015 Joshua Driesman, All rights reserved.
+ */
+@Controller
+public class EquipmentController {
+}
