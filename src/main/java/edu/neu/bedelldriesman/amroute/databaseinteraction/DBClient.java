2a7f078edded0aee1a4e8b392db8e3e95ffe6e26
Joshua Driesman <jdriesman@live.com>
2015-12-06 00:46:04 -0500

    Implemented getCitiesForSchedule

--- src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClient.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClient.java
@@ -1,11 +1,16 @@
 package edu.neu.bedelldriesman.amroute.databaseinteraction;
 
+import edu.neu.bedelldriesman.amroute.entitymodels.City;
 import edu.neu.bedelldriesman.amroute.entitymodels.Route;
+import edu.neu.bedelldriesman.amroute.entitymodels.Schedule;
+import edu.neu.bedelldriesman.amroute.entitymodels.Stop;
 
 import java.util.ArrayList;
 
 /**
  * Created by Joshua Driesman on 11/16/2015.
+ *
+ * Copyright 2015 Joshua Driesman, All rights reserved.
  */
 public interface DBClient {
     /*boolean create(Object input);
@@ -14,4 +19,10 @@ public interface DBClient {
     ArrayList<Route> getAllRoutes();
 
     ArrayList<Route> getServingRoutes(String cityName);
+
+    ArrayList<Schedule> getScheduleForRoute(String route);
+
+    ArrayList<Stop> getStopsForSchedule(int sId);
+
+    ArrayList<City> getCitiesForSchedule(int sId);
 }
--- src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClientImpl.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClientImpl.java
@@ -1,14 +1,21 @@
 package edu.neu.bedelldriesman.amroute.databaseinteraction;
 
+import edu.neu.bedelldriesman.amroute.entitymodels.City;
 import edu.neu.bedelldriesman.amroute.entitymodels.Route;
+import edu.neu.bedelldriesman.amroute.entitymodels.Schedule;
+import edu.neu.bedelldriesman.amroute.entitymodels.Stop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.SqlInOutParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
@@ -68,5 +75,57 @@ public class DBClientImpl implements DBClient {
         }
 
         return result;
+    }
+
+    @Override
+    public ArrayList<Schedule> getScheduleForRoute(String route) {
+        ArrayList<Schedule> result = new ArrayList<>();
+
+//        SimpleJdbcCall call = new SimpleJdbcCall(template)
+//                .withProcedureName("getSchedulesForRoute")
+//                .returningResultSet("rs1", new RowMapper<Object>() {
+//
+//                    @Override
+//                    public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+//                        return new Schedule(resultSet.getInt(0), resultSet.getInt(1), resultSet.getInt(2),
+//                                resultSet.getTime(3), resultSet.getTime(4), resultSet.getString(5));
+//                    }
+//                });
+
+        return null;
+    }
+
+    @Override
+    public ArrayList<Stop> getStopsForSchedule(int sId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<City> getCitiesForSchedule(int sId) {
+        ArrayList<City> cities = new ArrayList<>();
+
+        SimpleJdbcCall call = new SimpleJdbcCall(template)
+                .withProcedureName("getCitiesForSchedule")
+                .returningResultSet("rs1", (resultSet, i) ->
+                        new City(resultSet.getInt(0), resultSet.getString(1), resultSet.getString(2),
+                        resultSet.getString(3)));
+
+        SqlParameterSource in = new MapSqlParameterSource()
+                .addValue("sId", sId);
+
+        Object resultObject = call.execute(in).get("rs1");
+        ArrayList resultArrayList;
+
+        if (resultObject instanceof ArrayList) {
+            resultArrayList = (ArrayList) resultObject;
+        } else {
+            throw new IllegalStateException("Got unexpected stuff from database");
+        }
+
+        for (Object row : resultArrayList) {
+            cities.add((City) row);
+        }
+
+        return cities;
     }
 }
