eafdebfee645b4f893dc3409d0fbf69b2e302267
Joshua Driesman <jdriesman@live.com>
2015-12-06 17:21:52 -0500

    Initial commit for ScheduleController

---
+++ src/main/java/edu/neu/bedelldriesman/amroute/controllers/ScheduleController.java
@@ -0,0 +1,9 @@
+package edu.neu.bedelldriesman.amroute.controllers;
+
+/**
+ * Created by Joshua Driesman on 12/6/2015.
+ * <p>
+ * Copyright 2015 Joshua Driesman, All rights reserved.
+ */
+public class ScheduleController {
+}
