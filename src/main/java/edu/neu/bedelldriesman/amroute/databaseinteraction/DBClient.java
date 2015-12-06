1238532a51b3715183ae522cb0cb71b84188d916
Joshua Driesman <jdriesman@live.com>
2015-12-05 20:14:28 -0500

    Created getServingRoutes method

--- src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClient.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClient.java
@@ -1,10 +1,13 @@
 package edu.neu.bedelldriesman.amroute.databaseinteraction;
 
+import edu.neu.bedelldriesman.amroute.entitymodels.Route;
+
 /**
  * Created by Joshua Driesman on 11/16/2015.
  */
 public interface DBClient {
-    boolean create(Object input);
+    /*boolean create(Object input);
     boolean delete(int id);
-    Object get(int id);
+    Object get(int id);*/
+    Route getServingRoutes(String cityName);
 }
--- src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClientImpl.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/databaseinteraction/DBClientImpl.java
@@ -1,7 +1,13 @@
 package edu.neu.bedelldriesman.amroute.databaseinteraction;
 
+import edu.neu.bedelldriesman.amroute.entitymodels.Route;
+
 /**
  * Created by Joshua Driesman on 12/5/2015.
  */
-public class DBClientImpl {
+public class DBClientImpl implements DBClient {
+    @Override
+    public Route getServingRoutes(String cityName) {
+        return null;
+    }
 }
