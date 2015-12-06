ee98d8b7f8ec8f219605bad64d115ec52e05ba52
Joshua Driesman <jdriesman@live.com>
2015-12-06 15:09:47 -0500

    Implemented equipment controller

--- src/main/java/edu/neu/bedelldriesman/amroute/controllers/EquipmentController.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/controllers/EquipmentController.java
@@ -1,6 +1,16 @@
 package edu.neu.bedelldriesman.amroute.controllers;
 
+import edu.neu.bedelldriesman.amroute.databaseinteraction.DBClientImpl;
+import edu.neu.bedelldriesman.amroute.entitymodels.Equipment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 /**
  * Created by Joshua Driesman on 12/6/2015.
@@ -9,4 +19,18 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class EquipmentController {
+    @Autowired
+    JdbcTemplate temp;
+
+    @RequestMapping(path = "/route/equipment", method = RequestMethod.GET)
+    public String getEquipment(@RequestParam(value = "routeId", required = true) String routeId, Model model){
+        DBClientImpl client = new DBClientImpl(temp);
+
+        ArrayList<Equipment> equipment = client.getEquipmentForRoute(routeId);
+
+        model.addAttribute("equipment", equipment.toArray());
+
+        return "equipment";
+    }
+
 }
