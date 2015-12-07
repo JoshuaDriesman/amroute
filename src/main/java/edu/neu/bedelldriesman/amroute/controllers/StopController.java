e14db2cc766e0d6f2f0213f7a1e673d204346eed
Joshua Driesman <jdriesman@live.com>
2015-12-06 20:25:23 -0500

    Added delete stop

--- src/main/java/edu/neu/bedelldriesman/amroute/controllers/StopController.java
+++ src/main/java/edu/neu/bedelldriesman/amroute/controllers/StopController.java
@@ -1,9 +1,30 @@
 package edu.neu.bedelldriesman.amroute.controllers;
 
+import edu.neu.bedelldriesman.amroute.databaseinteraction.DBClient;
+import edu.neu.bedelldriesman.amroute.databaseinteraction.DBClientImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Joshua Driesman on 12/6/2015.
  * <p>
  * Copyright 2015 Joshua Driesman, All rights reserved.
  */
+
+@RestController
 public class StopController {
+    @Autowired
+    JdbcTemplate template;
+
+    @RequestMapping(path = "/stop/delete", method = RequestMethod.POST)
+    public void deleteStop(@RequestParam(name = "scheduleId") int scheduleId,
+                             @RequestParam(name = "cityId") int cityId) {
+        DBClient client = new DBClientImpl(template);
+        client.deleteStop(scheduleId, cityId);
+    }
 }
